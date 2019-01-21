b52dfc557e5483db271939714964afbe3930c45f
mattshardman <mattshardman@gmail.com>
2019-01-21 11:38:28 +0000

    complete stretch function conversion

--- assignments/objects.js
+++ assignments/objects.js
@@ -91,27 +91,29 @@ console.log(antonietta.multiplyNums(3,4));
 const parent = {
   name: "Susan",
   age: 70,
-  speak: function () { console.log(`Hello I'm ${this.name}`) },
+  speak: function () { return `Hello I'm ${this.name}`; },
   child: {
     name: "George",
     age: 50,
-    speak: function () { console.log(`Hello I'm ${this.name}`) },
+    speak: function () { return `Hello I'm ${this.name}`; },
     grandchild: {
       name: "Sam",
       age: 30,
-      speak: function () { console.log(`Hello I'm ${this.name}`) },
+      speak: function () { return `Hello I'm ${this.name}`; },
     }
   }
 }
 
+const { child, child: { grandchild }} = parent; 
 // Log the parent object's name
-
+console.log(parent.name);
 // Log the child's age
-
+console.log(child.age);
 // Log the name and age of the grandchild
-
+console.log(grandchild.age, grandchild.name);
 // Have the parent speak
-
+console.log(parent.speak());
 // Have the child speak
-
+console.log(child.speak());
 // Have the grandchild speak
+console.log(grandchild.speak());
